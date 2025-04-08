244578c22ba20c1e0712323cecfdb0cca32c9d0a
Eduardo-BGarcia <eduardobgarciax134abc39@gmail.com>
2025-04-07 21:30:31 -0300

    Criação de funções para alterar o idioma da página.

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,66 @@
+document.addEventListener("DOMContentLoaded", (event)=>{
+    buscarInscritos();
+    construirModal();    
+
+    const temaLocal = localStorage.getItem("tema");
+    document.body.setAttribute("data-theme", temaLocal);
+});
+
+let idiomaAtual = "pt";
+
+function alterarIdioma() {
+    idiomaAtual = idiomaAtual == "pt" ? "en" : "pt";
+    carregarIdioma(idiomaAtual);
+}
+
+function carregarIdioma(idioma){
+    fetch(`json/${idioma}.json`).then(data => data.json()).then(data =>{traduzirPagina(data);});
+}
+
+function traduzirPagina(linguagem){
+    document.querySelectorAll("[data-i18n]").forEach(elemento =>{
+        console.log(elemento);
+        const chave = elemento.getAttribute("data-i18n");
+        console.log(chave);
+        if(linguagem[chave]){
+            elemento.textContent = linguagem[chave];
+        }
+        //para imagens
+        
+    });
+}
+
+function construirModal(){
+    const botaoSaibaMais = document.getElementById("saiba-mais");
+    const modal = document.getElementById("modal");
+    const fecharModal = document.getElementById("fechar-modal");
+    botaoSaibaMais.addEventListener("click", ()=> {
+        modal.classList.remove("hidden");
+    });
+    
+    window.addEventListener("click", (e) =>{
+        console.log(e.target);
+        if(e.target == modal){
+            modal.classList.add("hidden");
+        }
+    });
+
+    fecharModal.addEventListener("click", (e)=>{
+        modal.classList.add("hidden");
+    })
+
+}
+
 function alterarTema() {
     //DOM -> document objetc model
     const tema = document.body.getAttribute("data-theme");
     //Se tema = dark, muda novoTema para light, se não, muda
     //novoTema para dark
     const novoTema = tema == "dark" ? "light" : "dark";
+    localStorage.setItem("tema", novoTema);
     document.body.setAttribute("data-theme", novoTema);
     mudarImagem(novoTema);
+    
 }
 
 function mudarImagem(tema) {
@@ -19,12 +74,15 @@ function mudarImagem(tema) {
 
 }
 function buscarInscritos(){
-    fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then(res=> {const divInscritos = document.getElementById("inscritos");
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("/json/inscritos.json").then(res => res.json()).then(res=> {const divInscritos = document.getElementById("inscritos");
         res.forEach(user => {
             const novoParagrafo = document.createElement("p");
-            novoParagrafo.textContent = `Nome: ${user.name}`;
+            novoParagrafo.textContent = `Nome: ${user.nome}`;
             divInscritos.appendChild(novoParagrafo);
         });
         
     }).catch(e=>console.log(e));
+
+    // alert("Olá");
 }
